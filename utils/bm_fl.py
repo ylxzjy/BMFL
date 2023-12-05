daafff0f332f39c86ab326916d679ac735208f41
ylxzjy <134262223+ylxzjy@users.noreply.github.com>
2023-12-05 21:24:03 +0800

    Update bm_fl.py

--- utils/bm_fl.py
+++ utils/bm_fl.py
@@ -12,7 +12,7 @@ def bm_fl(args, FL, getdata):
         #bm_flmnist(args, FL, getdata)
         bm_flcifar10(args, FL, getdata)
 
-#8fashion的尺寸大小和mnist相同使用和mnist一样的模型
+
 def bm_flmnist(args, FL, getdata):
     acc_list = []
     train_loss_list = []
@@ -21,7 +21,7 @@ def bm_flmnist(args, FL, getdata):
     print("dataset：{}   num_its:{}   ".format(args.dataset, args.num_its))
     global_dnet, global_gnet, ITSs_gnet, ITSs_dnet, optimizerg, optimizerd = FL.get_acmnistmodel()
 
-    #加载每个客户端持有的数据集：getdata入手
+    # load dataset：
     client_loader = []
     test_loader = None
     
@@ -30,53 +30,53 @@ def bm_flmnist(args, FL, getdata):
     print(len(client_loader))
     ITSs_lastdnet = []
 
-    # 创建client
+    # create client
     clients = []
     for idx in range(args.num_clients):
         client = Client(idx,client_loader[idx])
         clients.append(client)
     print("client create finished")
-    # 创建ITS
+    # create ITS
     itss = []
     for idx in range(args.num_its):
         its_clients = [obj for idj, obj in enumerate(clients) if idj % args.num_its == idx]
         its = TrustedServer(its_clients)
         itss.append(its)
     print("itss create finished")
-    del clients
-    # 预训练中间信任服务器
-    # for idx in range(args.num_its):
-    #     print("num of args.num_its :  ", idx)
-    #     loss = FL.client_train_preacgan_mnist(ITSs_gnet[idx], ITSs_dnet[idx],optimizerg[idx], optimizerd[idx], client_loader[idx])
+    del clients 
+    # pretrain in ITS
+    for idx in range(args.num_its):
+         print("num of args.num_its :  ", idx)
+         loss = FL.client_train_preacgan_mnist(ITSs_gnet[idx], ITSs_dnet[idx],optimizerg[idx], optimizerd[idx], client_loader[idx])
 
     print("pretrain finished")
 
     for idx in range(args.num_its):
         ITSs_lastdnet.append(ITSs_dnet[idx])
 
-    # 联邦训练中间信任服务器和中心服务器
+    # bm-fl
     for i in range(args.epoch):
         train_loss = 0
         for idx in range(args.num_its):
             loss = FL.client_train_bmfl(ITSs_gnet[idx], ITSs_dnet[idx], optimizerg[idx], optimizerd[idx], ITSs_lastdnet[idx],itss[idx])
             train_loss += loss
         
-        #聚合下发判别器
+        # Aggregation Downstream Discriminator
         FL.FedAvg(global_dnet, [ITSs_dnet[idx] for idx in range(args.num_its)])
         FL.updata_model(global_dnet, ITSs_dnet)
 
-        #聚合下发生成器
+        #Aggregation Downstream Generator
         FL.FedAvg(global_gnet, [ITSs_gnet[idx] for idx in range(args.num_its)])
         FL.updata_model(global_gnet, ITSs_gnet)
         
-        #每个中间信任服务器各自生成样本
+        # Generate samples
         for idx in range(args.num_its):
             FL.sample_mnistimage(args.num_classes, i*1000+idx, ITSs_gnet[idx])
 
-        #聚合生成器生成样本
+        # Aggregation generator generates samples
         FL.sample_mnistimage(args.num_classes, i*1000+args.num_its, global_gnet)
 
-        #测试聚合的判别器模型
+        # Test Aggregation Discriminator Model
         test_loss, accuracy = FL.test(global_dnet, test_loader)
         train_loss /= args.num_its
         accuracy = float(accuracy)
@@ -108,7 +108,7 @@ def bm_flcifar10(args, FL, getdata):
     print("dataset：{}   num_its:{}   ".format(args.dataset, args.num_its))
     global_dnet, ITSs_gnet, ITSs_dnet, optimizerg, optimizerd = FL.get_accifar10model()
 
-    #加载每个客户端持有的数据集：getdata入手
+    # load dataset
     client_loader = []
     test_loader = None
     
@@ -117,20 +117,20 @@ def bm_flcifar10(args, FL, getdata):
     print(len(client_loader))
     ITSs_lastdnet = []
 
-    # 创建client
+    # create client
     clients = []
     for idx in range(args.num_clients):
         client = Client(idx,client_loader[idx])
         clients.append(client)
     print("client create finished")
-    # 创建ITS
+    # create ITS
     itss = []
     for idx in range(args.num_its):
         its_clients = [obj for idj, obj in enumerate(clients) if idj % args.num_its == idx]
         its = TrustedServer(its_clients)
         itss.append(its)
     print("itss create finished")
-    # 预训练中间信任服务器
+    # pretrain in ITS
     # for idx in range(args.num_its):
     #     print("num of args.num_its :  ", idx)
     #     loss = FL.client_train_preacgan_cifar(ITSs_gnet[idx], ITSs_dnet[idx],optimizerg[idx], optimizerd[idx], client_loader[idx])
@@ -140,7 +140,7 @@ def bm_flcifar10(args, FL, getdata):
     for idx in range(args.num_its):
         ITSs_lastdnet.append(ITSs_dnet[idx])
 
-    # 联邦训练中间信任服务器和中心服务器
+    # bm-fl
     for i in range(args.epoch):
         train_loss = 0
         for idx in range(args.num_its):
@@ -150,11 +150,11 @@ def bm_flcifar10(args, FL, getdata):
         FL.FedAvg(global_dnet, [ITSs_dnet[idx] for idx in range(args.num_its)])
         FL.updata_model(global_dnet, ITSs_dnet)
         
-        #每个中间信任服务器各自生成样本
+        #generate samples
         for idx in range(args.num_its):
             FL.sample_cifarimage(args.num_classes, i*1000+idx, ITSs_gnet[idx])
 
-        #测试聚合的判别器模型
+        #Test Aggregation Discriminator Model
         test_loss, accuracy = FL.testcifar10(global_dnet, test_loader)
         train_loss /= args.num_its
         accuracy = float(accuracy)
